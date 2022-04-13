ff2d8fb1951a9bdfa25dcf99f1fe058e3aafebe5
Tiger <ding@tiger.work>
2022-04-12 18:48:27 -0700

    Update Swmac.py
    better performance/gui

--- Swmac.py
+++ Swmac.py
@@ -1,227 +1,126 @@
-import tkinter as tk
-from tkinter import ttk
 from mojang import MojangAPI
 from PIL import Image, ImageTk
-import io
 import requests
 import json
+import shutil
+from pathlib import Path
+import tkinter as tk
+from tkinter.constants import *
+from tkinter.scrolledtext import ScrolledText
 
-class inputGetErr(Exception):
-    pass
-class cantFind(Exception):
-    pass
-class imageDownloadErr(Exception):
-    pass
-class rowColumnArrangeErr(Exception):
-    pass
-class imagePlaceErr(Exception):
-    pass
-class imageIndiviCanvasErr(Exception):
-    pass
-class readJsonErr(Exception):
-    pass
-class printJsonErr(Exception):
-    pass
-
-def superAlth(Alst):
-    colNumber = 15
-    new = []
-    while len(Alst) > colNumber:
-        innerLst = []
-        for i in range(colNumber):
-            innerLst.append(Alst[i])
-        new.append(innerLst)
-        for i in range(colNumber):
-            Alst.pop(0)
-    lastList = []
-    for i in range(len(Alst)):
-        lastList.append(Alst[0])
-        Alst.pop(0)
-    new.append(lastList)
-    return new
-
-def addToChrList(nameID="TkinterDefault"):
-    try:
-        try:
-            if nameID == "TkinterDefault":
-                usrIn = uuidEntry.get()
-            else:
-                usrIn = nameID
-        except:
-            raise inputGetErr
-        try:
-            profile = MojangAPI.get_profile(usrIn)
-            if profile.name != "":
-                playerIDLst.append(MojangAPI.get_profile(usrIn).name)
-                playerUuidVar.set("\n".join(playerIDLst))
-                getPor(usrIn, playerIDLst[:])
-                mcFampor.update()
-        except:
-            try:
-                usrName = MojangAPI.get_uuid(usrIn)
-                usrIn = usrName
-                playerIDLst.append(MojangAPI.get_profile(usrIn).name)
-                playerUuidVar.set("\n".join(playerIDLst))
-                getPor(usrIn, playerIDLst[:])
-                mcFampor.update()
-            except:
-                raise cantFind
-    except inputGetErr:
-        errorVar.set("inputGetErr: Check your entry box")
-    except cantFind:
-        errorVar.set("cantFind: info not found in database")
-    except imageDownloadErr:
-        errorVar.set("imageDownloadErr: Image Download Error")
-    except rowColumnArrangeErr:
-        errorVar.set("rowColumnArrangeErr")
-    except imagePlaceErr:
-        errorVar.set("imagePlaceErr")
-    except imageIndiviCanvasErr:
-        errorVar.set("imageIndiviCanvasErr")
-
-def canvasPutDownForLst(fiveLst):
-    for row in range(len(fiveLst)):
-            for col in range(len(fiveLst[row])):
-                rowFrame = row
-                columnFrame = col
-                uuid = fiveLst[row][col]
-                
-                try:
-                    url = "https://crafatar.com/avatars/" + uuid
-                    imageTKForm = ImageTk.PhotoImage(Image.open(io.BytesIO(requests.get(url).content)).resize((40, 40)))
-                    try:
-                        imageIndiviCanvas = tk.Frame(picFrame)
-                        imageIndiviCanvas.grid(column=columnFrame, row=rowFrame)
-                    except:
-                        raise imageIndiviCanvasErr
-                        
-                    try:
-                        
-                        imageInfo = tk.Label(imageIndiviCanvas, image=imageTKForm)
-                        imageInfo.image = imageTKForm
-                        imageInfo.grid(column=0, row=0)
-                        
-                        try:
-                            imageName = tk.Label(imageIndiviCanvas, text=MojangAPI.get_profile(uuid).name, width=10, height=3)
-                            imageName.grid(column=0, row=1)
-                            mcFampor.update()
-                        except:
-                            imageName = tk.Label(imageIndiviCanvas, text="Invalid User", width=10, height=3)
-                            imageName.grid(column=0, row=1)
-                            mcFampor.update()
-                            errorVar.set("invalidName")
-                        
-                    except:
-                        raise imagePlaceErr
-                except:
-                    errorVar.set("imageDownloadErr: Image Download Error")
-                
-                
-
-def getPor(uuid, playerLst):
-    try:
-        url = "https://crafatar.com/avatars/" + uuid
-        imageTKForm = ImageTk.PhotoImage(Image.open(io.BytesIO(requests.get(url).content)).resize((40, 40)))
-    except:
-        raise imageDownloadErr
-    
-    try:
-        # print(playerLst)
-        fiveLst = superAlth(playerLst)
-        # print(fiveLst)
-        for row in range(len(fiveLst)):
-            for col in range(len(fiveLst[row])):
-                if fiveLst[row][col] == MojangAPI.get_profile(uuid).name:
-                    rowFrame = row
-                    columnFrame = col
-    except imageDownloadErr:
-        print("imageDownloadErr")
-    except imagePlaceErr:
-        print("imagePlaceErr")
-    except imageIndiviCanvasErr:
-        print("imageIndiviCanvasErr")
-    except:
-        raise printJsonErr
-    
-    try:
-        imageIndiviCanvas = tk.Frame(picFrame)
-        imageIndiviCanvas.grid(column=columnFrame, row=rowFrame)
-    except:
-        raise imageIndiviCanvasErr
+class Swmac:
+    def __init__(self, allowedExtensions):
+        self.root = tk.Tk()
+        projName = "Swmac v1.0.0"
+        self.root.title(projName)
+        self.allowedExtensions = allowedExtensions
+        self.windowInit()
+        self.frameBuild()
+        self.root.mainloop()
         
-    try:
-        imageInfo = tk.Label(imageIndiviCanvas, image=imageTKForm)
-        imageInfo.image = imageTKForm
-        imageInfo.grid(column=0, row=0)
-        imageName = tk.Label(imageIndiviCanvas, text=MojangAPI.get_profile(uuid).name, width=10, height=3)
-        imageName.grid(column=0, row=1)
-    except:
-        raise imagePlaceErr
-    
+    def windowInit(self):
+        self.root.update_idletasks()
+        width = 710
+        frm_width = self.root.winfo_rootx() - self.root.winfo_x()
+        self.root_width = width + 2 * frm_width
+        height = 600
+        titlebar_height = self.root.winfo_rooty() - self.root.winfo_y()
+        self.root_height = height + titlebar_height + frm_width
+        x = self.root.winfo_screenwidth() // 2 - self.root_width // 2
+        y = self.root.winfo_screenheight() // 2 - self.root_height // 2
+        self.root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        self.root.deiconify()
+        self.root.resizable(False, False)
 
-def jsonSearch():
-    uuidLst = []
-    openF = "/Users/tiger/Downloads/whitelist.json"
-    # openF = uuidEntry.get()
-    try:
-        try:
-            with open(openF,'r') as jsonFile:
-                loadDict = json.load(jsonFile)
-                for i in loadDict:
-                    uuidLst.append(i["uuid"])
-        except:
-            raise readJsonErr
-        try:
-            finalA = superAlth(uuidLst)
-            canvasPutDownForLst(finalA)
-        except:
-            raise printJsonErr
-    except readJsonErr:
-        print("readJsonErr")
+    def frameBuild(self):
+        self.errorVar = tk.StringVar()
+        self.errorVar.set("Error: None")
+        self.playerUuidVar = tk.StringVar()
+        self.playerUuidVar.set("")
+        self.displayFrame = tk.Frame(self.root, padx=20)
+        self.displayFrame.pack(fill="both", expand=True, anchor="n")
+        self.userPromptLabelFrame = tk.LabelFrame(self.root, text="Swmac Console", padx=10, pady=10)
+        self.userPromptLabelFrame.pack(pady=20)
+        self.inputFrame = tk.Frame(self.userPromptLabelFrame)
+        self.inputFrame.pack()
+        self.additionFrame = tk.Frame(self.userPromptLabelFrame)
+        self.additionFrame.pack()
+        if True:
+            self.mainDisplayArea = ScrolledText(self.displayFrame, wrap=WORD)
+            self.mainDisplayArea.pack(fill="both", expand=True)
+        if True:
+            # userPromptLabelFrame design
+            self.instructionLabelA = tk.Label(self.inputFrame, text=".JSON Path:")
+            self.instructionLabelA.pack(side="left")
+            self.uuidEntryBox = tk.Entry(self.inputFrame, width=52)
+            self.uuidEntryBox.insert(0, "/Users/tiger/Downloads/whitelist.json")
+            self.uuidEntryBox.pack(side="left")
+            self.filePathAutoSearcher = tk.Button(self.inputFrame, text="Execute", command=self.mainFunc, width=5)
+            self.filePathAutoSearcher.pack(side="left")
+            
+            self.instructionLabelB = tk.Label(self.additionFrame, text="Folder Path:")
+            self.instructionLabelB.pack(side="left")
+            self.projFolderEntryBox = tk.Entry(self.additionFrame, width=52)
+            self.projFolderEntryBox.insert(0, "/Users/tiger/Downloads/mapiot")
+            self.projFolderEntryBox.pack(side="left")
+            self.quitButton = tk.Button(self.additionFrame, text="Quit", command=quit, width=5)
+            self.quitButton.pack(side="left")
+        if True:
+            self.mainDisplayArea.downloadedFiles = []
+            self.mainDisplayArea.images = []
+
+    def mainFunc(self):
+        self.projFolder = self.projFolderEntryBox.get()
+        uuidLst = []
+        openF = self.uuidEntryBox.get()
+        with open(openF,'r') as jsonFile:
+            loadDict = json.load(jsonFile)
+            for i in loadDict:
+                uuidLst.append(i["uuid"])
+        avaUsr = []
+        for i in uuidLst:
+            try:
+                aUselessVar = MojangAPI.get_profile(i).name
+                filePath = self.projFolder + "/avaImg_" + str(len(avaUsr)) + ".png"
+                with open(filePath, "wb") as outFile:
+                    url = "https://crafatar.com/avatars/" + i
+                    response = requests.get(url, stream=True)
+                    shutil.copyfileobj(response.raw, outFile)
+                del response
+                avaUsr.append(i)
+            except:
+                pass
+        self.mainDisplayArea.downloadedFiles.clear()
+        for filepath in Path(self.projFolder).iterdir():
+            if filepath.suffix in self.allowedExtensions:
+                self.mainDisplayArea.insert(INSERT, filepath.name+'\n')
+                self.mainDisplayArea.downloadedFiles.append(filepath)
+        self.mainDisplayArea.delete('1.0', END)
+        self.mainDisplayArea.images.clear()
+        for avatarImageName in self.mainDisplayArea.downloadedFiles:
+            if str(avatarImageName)[-6:-5] == "_":
+                idIndex = str(avatarImageName)[-5:-4]
+            else:
+                idIndex = str(avatarImageName)[-6:-4]
+            uuidToPrint = avaUsr[int(idIndex)]
+            img = Image.open(avatarImageName).resize((20, 20))
+            img = ImageTk.PhotoImage(img)
+            idHistory = MojangAPI.get_name_history(uuidToPrint)
+            idHistoryLst = []
+            for data in idHistory:
+                idHistoryLst.append(data['name'])
+            idHistoryLst.pop(-1)
+            if len(idHistoryLst) == 0:
+                idHistoryLst.append("Current")
+            else:
+                pass
+            self.mainDisplayArea.image_create(INSERT, padx=5, pady=5, image=img)
+            self.mainDisplayArea.images.append(img)
+            self.mainDisplayArea.insert(INSERT, f"{MojangAPI.get_profile(uuidToPrint).name:<20}")
+            self.mainDisplayArea.insert(INSERT, f"{str('|'):<3}{uuidToPrint}   ")
+            self.mainDisplayArea.insert(INSERT, f"{str('|'):<3}{'>>'.join(idHistoryLst[-1:])} >>")
+            self.mainDisplayArea.insert(INSERT, '\n')  
 
 if __name__ == "__main__":
-    
-    projName = "Server Whitelist to Minecraft Avatars Converter"
-    mcFampor = tk.Tk()
-    mcFampor.title(projName)
-    mcFampor.geometry("1920x1080")
-    
-    bigFrame = tk.Frame(mcFampor)
-    bigFrame.pack(fill="both", expand=1)
-    scrollCan = tk.Canvas(bigFrame)
-    scrollCan.pack(side="left", fill="both", expand=1)
-    scrollMain = ttk.Scrollbar(bigFrame, orient="vertical", command=scrollCan.yview)
-    scrollMain.pack(side="right", fill="y")
-    scrollCan.configure(yscrollcommand=scrollMain.set)
-    scrollCan.bind("<Configure>", lambda e: scrollCan.configure(scrollregion=scrollCan.bbox("all")))
-    secondFrame = tk.Frame(scrollCan)
-    scrollCan.create_window((950,0), window=secondFrame, anchor="n")
-    
-    errorVar = tk.StringVar()
-    errorVar.set("")
-    playerUuidVar = tk.StringVar()
-    playerUuidVar.set("")
-    
-    picFrame = tk.Frame(secondFrame)
-    mainFrame = tk.Frame(secondFrame)
-    picFrame.pack()
-    mainFrame.pack()
-    
-    playerIDLst = []
-    
-    uuidEntry = tk.Entry(mainFrame, text="Enter UUID")
-    addToCharacterList = tk.Button(mainFrame, text="Add to List", command=addToChrList)
-    errorLabel = tk.Label(mainFrame, textvariable=errorVar)
-    playerLabel = tk.Label(mainFrame, textvariable=playerUuidVar)
-    quitButton = tk.Button(mainFrame, text="Quit", command=quit)
-    filePathAutoSearcher = tk.Button(mainFrame, text="Json Whitelist Search", command=jsonSearch)
-    
-    # playerLabel.pack()
-    errorLabel.pack()
-    uuidEntry.pack()
-    addToCharacterList.pack()
-    filePathAutoSearcher.pack()
-    
-    quitButton.pack()
-    
-    mcFampor.mainloop()
+    allowedExtensions = {'.jpg', '.png'}
+    Swmac(allowedExtensions)
